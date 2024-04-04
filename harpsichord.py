6595359e70d4200d02a797ecf99a9c00b8059616
Frolenko Ruslan <heckfyferus@gmail.com>
2024-04-04 20:21:03 +0700

    добавил вывод команд перемешаных случайным образом - по строчно

--- harpsichord.py
+++ harpsichord.py
@@ -1,9 +1,30 @@
+# импортируем необходимые библиотеки
+import random
+
+# получить и подготовить данные
 with open('git.txt','r',encoding='utf-8') as file:
     data=[[k.strip() for k in i.split('#')] for i in file.read().splitlines()]
-    for i in data:
-        print(i[1])
+
+
+# вывести на экран построчно весь список команд использую клвишу ENTER
+input('нажимайте клавишу клавесина ENTER для построчного вывода команд, если Вам надоест смотреть \nкоманды Вы всегда можете выдернуть шнур компьютера из розетки или написть - ХВАТИТ')
+print()
+while True:
+    n=random.choice(data)
+    print(n[0])
+    print(n[1])
+    if input()=='ХВАТИТ':
+        break
+   
+
+ 
+        
+
+
+
 
     
+
 
 
 
