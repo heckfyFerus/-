bb65175e3b36273030cdd40d1feb503a16c0f57f
Frolenko Ruslan <heckfyferus@gmail.com>
2024-04-07 23:31:49 +0700

    Создана и протестирована функция Training

--- harpsichord.py
+++ harpsichord.py
@@ -8,24 +8,26 @@ with open('git.txt','r',encoding='utf-8') as file:
     data=[[k.strip() for k in i.split('#')] for i in file.read().splitlines()]
 
 
+# эта функция выводит в случайном порядке в одном и том же месте терминала (не сдвигаю сроки вниз) на первой строке комментарий, а на второй код
+def training():
+    input('Вы находитесь в тренировочном режиме, нажимайте клавишу клавесина ENTER для построчного вывода команд, если Вам надоест смотреть \nкоманды Вы всегда можете выдернуть шнур компьютера из розетки или написть - ХВАТИТ')
+    print()
+    while True:
+        n=random.choice(data)
+        comment = n[1]
+        code = n[0]
+        sys.stdout.write(f"{comment}\n")
+        sys.stdout.write(f"{code}\n")
+        sys.stdout.flush()
+        if input()=='ХВАТИТ':
+            break
+        sys.stdout.write('\033[F' * 3)  # Возврат курсора на три строки назад
+        sys.stdout.write(f" {len(comment)*' '}\n")  
+        sys.stdout.write(f" {len(code)*' '}\n")
+        sys.stdout.write('\033[F' * 2)
 
-input('Нажимайте клавишу клавесина ENTER для построчного вывода команд, если Вам надоест смотреть \nкоманды Вы всегда можете выдернуть шнур компьютера из розетки или написть - ХВАТИТ')
-print()
-while True:
-    n=random.choice(data)
-    comment = n[1]
-    code = n[0]
-    sys.stdout.write(f"{comment}\n")
-    sys.stdout.write(f"{code}\n")
-    sys.stdout.flush()
-    if input()=='ХВАТИТ':
-        break
-    sys.stdout.write('\033[F' * 3)  # Возврат курсора на три строки назад
-    sys.stdout.write(f" {len(comment)*' '}\n")  
-    sys.stdout.write(f" {len(code)*' '}\n")
-    sys.stdout.write('\033[F' * 2)
 
-
+training()
 
  
         
