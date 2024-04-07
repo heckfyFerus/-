e361cb4ca15e9c1e23ff0e30a85f7801bf0711e9
Frolenko Ruslan <heckfyferus@gmail.com>
2024-04-08 00:29:17 +0700

    продолжаю работу над функцией battle

--- harpsichord.py
+++ harpsichord.py
@@ -18,7 +18,7 @@ def training():
         code = n[0]
         sys.stdout.write(f"{comment}\n")
         sys.stdout.write(f"{code}\n")
-        sys.stdout.flush()
+        # sys.stdout.flush() программа должна работать без этой команды - проверить
         if input()=='ХВАТИТ':
             break
         sys.stdout.write('\033[F' * 3)  # Возврат курсора на три строки назад
@@ -27,7 +27,41 @@ def training():
         sys.stdout.write('\033[F' * 2)
 
 
-training()
+def battle():
+    input('''Вы находитесь в боевом режиме, нажмите клавишу клавесина ENTER, берите в руки клавиатуру и гребите к берегу,
+и пусть вам повезет в бесконечном цикле. Если все надоест напишите - ХВАТИТ ''')
+    print()
+    while True:
+        n=random.choice(data)
+        comment = n[1]
+        code = n[0]
+        sys.stdout.write(f"{comment}\n")
+        sys.stdout.write(f"{code}\r")
+        int=input()
+        if int=='ХВАТИТ':
+            break
+        if int==code:
+            sys.stdout.write('\033[F' * 2)  # Возврат курсора на две строки назад
+            sys.stdout.write(f" {len(comment)*' '}\n")  
+            sys.stdout.write(f" {len(code)*' '}\n")
+            sys.stdout.write('\033[F' * 2)
+        else:
+            while True:
+                sys.stdout.write('\033[F' * 2)  # Возврат курсора на две строки назад
+                sys.stdout.write(f" {len(comment)*' '}\n")  
+                sys.stdout.write(f" {len(code)*' '}\n")
+                sys.stdout.write('\033[F' * 2)
+                sys.stdout.write(f"{comment}\n")
+                sys.stdout.write(f"{code}\r")
+                int=input()
+                if int==code:
+                    break
+                if int=='ХВАТИТ':
+                    break
+
+
+
+battle()
 
  
         
